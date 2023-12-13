b1962e257c5b23a59d0b28162f6805bfcfcaf10c
alval90 <alval90@users.noreply.github.com>
2023-12-13 14:34:31 +0100

    fix: csrf token not being set on initial render in private mode

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,12 @@
 """This module defines the url patterns and views to render"""
 from django.contrib import admin
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.urls import re_path, path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+@ensure_csrf_cookie
 def index_view(request):
     """This function returns the HttpResponse created using the static build of the react app"""
     return render(request, 'dist/index.html')
